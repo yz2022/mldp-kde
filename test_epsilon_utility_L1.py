3efb28d6da2db2056dfe5f20cdd2478ca91ad4cd
YiZhou <59765776+yz2022@users.noreply.github.com>
2023-12-11 01:39:43 +0800

    Update test_epsilon_utility_L1.py

--- test_epsilon_utility_L1.py
+++ test_epsilon_utility_L1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from draw_plot import draw_epsilon_MSE_l1
+from plotting_tools import draw_epsilon_MSE_l1
 from PM import piecewise_l1_l2_kernel_kde
 from RACE import count_race_l1
 from DM import duchi_l1_l2_kernel_kde
